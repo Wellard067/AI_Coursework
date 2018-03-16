97217157b1ac3f29866bc2d73a599206e853862d
dannywong1997 <36736271+dannywong1997@users.noreply.github.com>
2018-03-16 11:21:35 +0000

    Add files via upload
    updated header

--- include/Henry.h
+++ include/Henry.h
@@ -14,28 +14,37 @@ public:
 	void markBase(Position p);
 	void markShell(Position p);
 	void markEnemy(Position p);
-	void turretMachine();
+	void turretAimMachine();
+	void turretfiringMachine();
 	void tankMachine();
 	bool isFiring();
 	void score(int thisScore, int enemyScore);
 
 private:
-	enum TurretState { AIM, DETECTION, SHOOTINGTANK, SHOOTINGBASE, NOAMMO };
-	TurretState turretState = DETECTION;
+	enum TurretAimState { AIM, DETECTION, NOAMMO };
+	TurretAimState turretAimState = DETECTION;
+	enum TurretFiringState { SHOOTINGBASE, SHOOTINGTANK,LOOKINGFORTARGET};
+	TurretFiringState turretFiringState = LOOKINGFORTARGET;
+	enum TankMovementState { ROTATE, MOVINGTOWARD, IDLE, RUNAWAY,DODGESHELL };
+	TankMovementState tankMovementState = IDLE;
 	bool firing = false;
 	bool detectedEnemy = false;
+	bool detectedShell = false;
+	int ShellDetectedTimer = 0;
 	bool enemyIsDead = false;
-	float directionOfTarget = 0;
+	float angleBetweeThisAndTarget = 0;
 	//turrentMachine variables
 	bool baseIsDead = false;
-	float turrentFacingDirection = 0;
+	float targetRotationAngle = 0;
 	enum EnemyType {TANK,BASE,NOTHING};
 	EnemyType enemyType = NOTHING;
 	bool tankenDamageRecently = false;
+	bool detectingEnemy = true;
 	//tankMachine variables
 	float distanceBetweenThisAndTarget = 0;
 	float safeDistance = 120.f;
 	float tankFacingDirection = 0;
-	
+	bool turretHasTargetLocked = false;
+	int bearingDegrees = 0;
 };
 
